686ed9db1700f5e53f3a58d9e0acd01bcc70b9a3
khangvul <belo9063872680@gmail.com>
2021-09-10 17:35:55 +0300

    added virtualizated list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import List from "./List";
+import List from "./List.js";
 
 function App() {
   return (
     <div className="App" style={{ width: "200px", margin: "0 auto" }}>
       <List
-        rowCount={100}
+        rowCount={100000}
         rowHeight={32}
-        rowRenderer={(rowIndex) => `Record ${rowIndex}`}
+        rowRenderer={(index) => `Record ${index}`}
         width={200}
         height={600}
       />
--- src/List.js
+++ src/List.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 function List({ rowCount, rowHeight, rowRenderer, width, height }) {
-  return <div></div>;
+  const [scrollTop, setScrollTop] = useState(0);
+  //   const [items, setItems] = useState();
+
+  const innerHeight = rowCount * rowHeight;
+  const startIndex = Math.floor(scrollTop / rowHeight);
+  const endIndex = Math.min(
+    rowCount - 1,
+    Math.floor((scrollTop + height) / rowHeight)
+  );
+
+  //   const visibleItems = useMemo(() => {
+  //     const items = [];
+  //     for (let i = startIndex; i <= endIndex; i++) {
+  //       console.log("newItems");
+  //       items.push(
+  //         <li
+  //           style={{
+  //             position: "absolute",
+  //             top: `${i * rowHeight}px`,
+  //             height: rowHeight,
+  //           }}
+  //         >
+  //           {rowRenderer(i)}
+  //         </li>
+  //       );
+  //     }
+  //     setItems(items);
+  //   }, [startIndex, endIndex]);
+
+  const items = [];
+  for (let i = startIndex; i <= endIndex; i++) {
+    console.log("newItems");
+    items.push(
+      <li
+        style={{
+          position: "absolute",
+          top: `${i * rowHeight}px`,
+          height: rowHeight,
+        }}
+      >
+        {rowRenderer(i)}
+      </li>
+    );
+  }
+
+  const onScroll = (e) => setScrollTop(e.currentTarget.scrollTop);
+
+  return (
+    <div
+      style={{ overflow: "auto", height, border: "1px solid black", width }}
+      onScroll={onScroll}
+    >
+      <ul
+        className="inner"
+        style={{
+          position: "relative",
+          height: `${innerHeight}px`,
+        }}
+      >
+        <li>{items}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default List;
